80f16fec5d2ff5c5bcf446edd35288f2e9b27135
Diegoms228 <163172268+Diegoms228@users.noreply.github.com>
2026-03-04 20:25:36 +0100

    Add files via upload
    Signed-off-by: Diegoms228 <163172268+Diegoms228@users.noreply.github.com>

---
+++ mi-proyecto/1.js
@@ -0,0 +1,30 @@
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     const form = document.getElementById("loginForm")
+
+//     form.addEventListener("submit", function (event) {event.preventDefault();
+
+//         const usuario = document.getElementById("usuario").value;
+//         const password = document.getElementById("password").value;
+//         const email = document.getElementById("email").value;
+
+//         console.log("Usuario", usuario);
+//         console.log("Password", password);
+//         console.log("Email", email);
+//     });
+
+//     form.addEventListener("submit", function (event) {
+
+//         event.preventDefault();
+//         console.log("Formulario enviado");
+//         console.log("Input value:", input.value);
+//     });
+// });
+
+let buttom = document.getElementById("boton")
+// let inputValue = document.getElementById("")
+
+buttom.addEventListener("click", function() {
+    console.log("buttom clicked")
+    // console.log("Input value:", inputValue.value)
+});
